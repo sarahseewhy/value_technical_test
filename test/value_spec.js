73046bdea776ec979b21eb61529c7dda2178bef8
Sarah Young <y.sarahc@gmail.com>
2014-06-19 16:58:49 +0100

    Rephrasing tests to ensure comprehension

--- test/value_spec.js
+++ test/value_spec.js
@@ -5,32 +5,38 @@ test('Value is defined', function(t){
   t.type(Value, 'function');
   t.end();
 });
+// tests that Value is a function
 
 // test('Value has no enumerable properties', function(t){
 //   t.equal(Object.keys(Value()).length, 0);
 //   t.end();
 // });
+// testing to see if value contains an empty hash
 
 // test('property .value is defined', function(t){
 //   t.notEqual(Value({max: 1, value: Math.random()}), void 0);
 //   t.end();
 // });
+// tests that the value is not undefined
 
 // test('.value should be 0 by default', function(t){
 //   t.equal(Value().value, 0);
 //   t.end();
 // });
+// tests that value is 0 by default
 
 // test('.value can be assigned on instantiation', function(t){
 //   t.equal(Value({value: 2}).value, 2);
 //   t.end();
 // });
+// tests that a value can be set immediately when the function is called
 
 // test('.value is between 0 and 1', function(t){
 //   var val = Value({max: 1, value: Math.random()});
 //   t.ok(val.value <= 1, 'should create a value less than 1');
 //   t.end();
 // });
+// tests that the value within Value is between 0 and 1
 
 // test('setting to disallowed value throws error', function(t){
 //   var val = Value({max: 1, value: Math.random()});
@@ -41,6 +47,7 @@ test('Value is defined', function(t){
 //   }
 //   t.end();
 // });
+// tests that a value outside the max boundary throws an error
 
 // test('.value can be re-set within bounds', function(t){
 //   var val = Value({max: 5, value: 2});
@@ -48,6 +55,8 @@ test('Value is defined', function(t){
 //   t.equal(val.value, 4);
 //   t.end();
 // });
+
+// tests that the value's max boundary can be reset
 
 // test('.value cannot be set to below minimum', function(t){
 //   var val = Value({min: 5, value: 6});
@@ -58,3 +67,4 @@ test('Value is defined', function(t){
 //   }
 //   t.end();
 // });
+// tests that the value cannot be below the min boundary
