f9e58ed793cae819d72a32ca60d8c58e09fde534
Sarah Young <y.sarahc@gmail.com>
2014-06-21 14:41:14 +0100

    Created a function Value which contains a hash in which store a number

--- value.js
+++ value.js
@@ -1,4 +1,9 @@
-
+// Test 1 requires I create a function 'Value'
+function Value(options){
+	// I want to return a new value 'myValue' that can store a number 
+	// so I created a hash
+	var myValue = {};
+}
 
 module.exports = function(options) {
 	var result = new Value(options)
